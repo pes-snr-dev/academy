1932f063836816f3b3e09331f6d31cddf8244d52
xcixor <pndungu54@gmail.com>
2023-05-31 04:26:02 +0300

    fix(auth): ensure passwords are checked before login

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -6,13 +6,20 @@ import generateToken from "../utils/generateToken.js";
 // @access public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
-  if (user && user.matchPasswords(password)) {
+
+  if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
-    res.status(201).json({ _id: user._id, email: user.email });
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
   } else {
-    res.status(400);
-    throw new Error("Invalid credentials.");
+    res.status(401);
+    throw new Error("Invalid email or password");
   }
 });
 
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -30,17 +30,20 @@ const userSchema = mongoose.Schema(
   }
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password using bcrypt
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
     next();
   }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
-
-userSchema.methods.matchPasswords = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 const User = mongoose.model("User", userSchema);
 
