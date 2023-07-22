8a0d8e5b1f517f6e5927ef07f59072514b4d3b23
xcixor <pndungu54@gmail.com>
2023-07-22 08:24:27 +0300

    chore(courses): add course coach when saving data

--- api/routes/courseRoutes.js
+++ api/routes/courseRoutes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import fs from "fs-extra";
 import { createCourse } from "../controllers/courseController.js";
 import { uploadSingleFilesMiddleware } from "../middleware/uploadMiddleware.js";
 const router = express.Router();
