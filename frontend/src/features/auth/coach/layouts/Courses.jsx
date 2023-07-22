bff124f120793e1c4215b81cd92aa50cd7ab9bd6
xcixor <pndungu54@gmail.com>
2023-07-22 13:42:57 +0300

    chore(courses): set courses to state after fetch

--- frontend/src/features/auth/coach/layouts/Courses.jsx
+++ frontend/src/features/auth/coach/layouts/Courses.jsx
@@ -1,44 +1,17 @@
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { toast } from "react-toastify";
+import { useState } from "react";
 import { Wrapper, CreateCourseModal, CoursesList } from "../components";
-import { Card, Button } from "react-bootstrap";
-import { useGetCoachCoursesQuery } from "../../../../slices/coursesSlice";
-import { setCourses } from "../../../../slices/coachSlice";
+import { Button } from "react-bootstrap";
 
 const Courses = () => {
   const [show, setShow] = useState(false);
-  const dispatch = useDispatch();
-  // const { userInfo } = useSelector((state) => state.auth);
-  let coachPosts;
-  // try {
-  //   const {
-  //     data: courses,
-  //     isFetching,
-  //     isLoading,
-  //   } = useGetCoachCoursesQuery(userInfo._id, {
-  //     pollingInterval: 3000,
-  //     refetchOnMountOrArgChange: true,
-  //     skip: false,
-  //   });
-  //   coachPosts = courses;
-  // } catch (err) {
-  //   toast.error(err?.data?.message || err.error);
-  // }
-  // useEffect(() => {
-  //   dispatch(setCourses(coachPosts));
-  // }, [coachPosts, dispatch]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  // const { courses } = useSelector((state) => state.coach);
-  // console.log(courses);
 
   return (
     <Wrapper>
       <div className="soft-shadow">
         <h3>My Courses</h3>
-        {/* {courses ? && <CoursesList courses={courses} />} */}
         <CoursesList />
       </div>
       <CreateCourseModal show={show} handleClose={handleClose} />
