1adfe6c8e6c4d3b3232b1a2f29b7d05c522f0538
xcixor <pndungu54@gmail.com>
2023-10-05 13:09:15 +0300

    ignore files that should not leave the development environment

--- frontend/src/pages/ProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-import Profile from "../features/auth/components/profile";
-import { CoachProfile } from "../features/auth/coach";
-
-const ProfilePage = () => {
-  return <CoachProfile />;
-};
-
-export default ProfilePage;
--- frontend/src/pages/RegistrationPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Register from "../features/auth/components/Register";
-
-const RegistrationScreen = () => {
-  return <Register />;
-};
-
-export default RegistrationScreen;
