0bd2473febcf512747f8cfdca0614e717a9d0c37
xcixor <pndungu54@gmail.com>
2023-10-05 13:09:15 +0300

    ignore files that should not leave the development environment

--- frontend/src/features/auth/coach/index.js
+++
@@ -1,5 +0,0 @@
-export { default as CoachProfile } from "./layouts/Profile.jsx";
-export { default as Courses } from "./layouts/Courses.jsx";
-export { default as EditProfile } from "./layouts/EditProfile.jsx";
-export { default as EditCoursePage } from "./layouts/EditCourse.jsx";
-export { default as EditChapterPage } from "./layouts/EditChapterPage.jsx";
--- frontend/src/pages/LoginPage.jsx
+++
@@ -1,7 +0,0 @@
-import Login from "../features/auth/components/Login";
-
-const LoginScreen = () => {
-  return <Login />;
-};
-
-export default LoginScreen;
